0cecfeb0a1051d7a03032dbc17b92e67e8233228
Daniel Büchele <daniel@buechele.cc>
2012-12-27 14:50:45 +0100

    fixed import bug

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get(/event\/(.+)/, function(req, res) {
 app.get('/events', function(req, res) {
     db.events.find({},{"abstract":0, "description":0},function(err, docs) {
         if (err) res.send(500);
-        
+
         for (i=0;i<docs.length;i++) {
             
             duration = docs[i].duration.split(":");
@@ -98,46 +98,50 @@ app.post('/events',adminAuth, function(req, res) {
                 
                 for (i=0;i<eventJson.schedule.day.length;i++) {
                     for (j=0;j<eventJson.schedule.day[i].room.length;j++) {
-                        for (e=0;e<eventJson.schedule.day[i].room[j].event.length;e++) {
-                        
-                            obj = {};
-                            obj._id = eventJson.schedule.day[i].room[j].event[e].$.id;
-                            obj.conference = eventJson.schedule.conference[0].title[0];
-                            obj.date = eventJson.schedule.day[i].$.date;
-                            obj.location = eventJson.schedule.day[i].room[j].$.name;
-                            obj.start = eventJson.schedule.day[i].room[j].event[e].start[0];
-                            obj.duration = eventJson.schedule.day[i].room[j].event[e].duration[0];
-                            obj.room = eventJson.schedule.day[i].room[j].event[e].room[0];
-                            obj.slug = eventJson.schedule.day[i].room[j].event[e].slug[0];
-                            obj.title = eventJson.schedule.day[i].room[j].event[e].title[0];
-                            obj.subtitle = eventJson.schedule.day[i].room[j].event[e].subtitle[0];
-                            obj.track = eventJson.schedule.day[i].room[j].event[e].track[0];
-                            obj.type = eventJson.schedule.day[i].room[j].event[e].type[0];
-                            obj.language = eventJson.schedule.day[i].room[j].event[e].language[0];
+                        console.log("------");
+                        console.log(eventJson.schedule.day[i].room[j]);
+                        if (eventJson.schedule.day[i].room[j].event) {
+                            for (e=0;e<eventJson.schedule.day[i].room[j].event.length;e++) {
                             
-                            dateParts = obj.date.split("-");
-                            timeParts = obj.start.split(":");
-                            
-                            obj.timestamp = new Date(parseInt(dateParts[0]), (parseInt(dateParts[1]) - 1), parseInt(dateParts[2]),parseInt(timeParts[0])+1,parseInt(timeParts[1])).getTime()/1000;
-                            obj.persons = [];
-                            try {
-                                for (p=0;p<eventJson.schedule.day[i].room[j].event[e].persons[0].person.length;p++) {
-                                    obj.persons.push(eventJson.schedule.day[i].room[j].event[e].persons[0].person[p]["_"]);
-                                }
-                            } catch (e) {}
-                            
-                            obj.links = [];
-                            try {
-                                for (l=0;l<eventJson.schedule.day[i].room[j].event[e].links[0].link.length;l++) {
-                                    obj.links.push(eventJson.schedule.day[i].room[j].event[e].links[0].link[l].$.href);
-                                }
-                            } catch (e) {}
-                            
-                            
-                            obj.description = eventJson.schedule.day[i].room[j].event[e].description[0];
-                            obj.abstract = eventJson.schedule.day[i].room[j].event[e].abstract[0];
-                            events.push(obj);
-                            //db.events.save(obj, function(err, saved) {});
+                                obj = {};
+                                obj._id = eventJson.schedule.day[i].room[j].event[e].$.id;
+                                obj.conference = eventJson.schedule.conference[0].title[0];
+                                obj.date = eventJson.schedule.day[i].$.date;
+                                obj.location = eventJson.schedule.day[i].room[j].$.name;
+                                obj.start = eventJson.schedule.day[i].room[j].event[e].start[0];
+                                obj.duration = eventJson.schedule.day[i].room[j].event[e].duration[0];
+                                obj.room = eventJson.schedule.day[i].room[j].event[e].room[0];
+                                obj.slug = eventJson.schedule.day[i].room[j].event[e].slug[0];
+                                obj.title = eventJson.schedule.day[i].room[j].event[e].title[0];
+                                obj.subtitle = eventJson.schedule.day[i].room[j].event[e].subtitle[0];
+                                obj.track = eventJson.schedule.day[i].room[j].event[e].track[0];
+                                obj.type = eventJson.schedule.day[i].room[j].event[e].type[0];
+                                obj.language = eventJson.schedule.day[i].room[j].event[e].language[0];
+                                
+                                dateParts = obj.date.split("-");
+                                timeParts = obj.start.split(":");
+                                
+                                obj.timestamp = new Date(parseInt(dateParts[0]), (parseInt(dateParts[1]) - 1), parseInt(dateParts[2]),parseInt(timeParts[0])+1,parseInt(timeParts[1])).getTime()/1000;
+                                obj.persons = [];
+                                try {
+                                    for (p=0;p<eventJson.schedule.day[i].room[j].event[e].persons[0].person.length;p++) {
+                                        obj.persons.push(eventJson.schedule.day[i].room[j].event[e].persons[0].person[p]["_"]);
+                                    }
+                                } catch (e) {}
+                                
+                                obj.links = [];
+                                try {
+                                    for (l=0;l<eventJson.schedule.day[i].room[j].event[e].links[0].link.length;l++) {
+                                        obj.links.push(eventJson.schedule.day[i].room[j].event[e].links[0].link[l].$.href);
+                                    }
+                                } catch (e) {}
+                                
+                                
+                                obj.description = eventJson.schedule.day[i].room[j].event[e].description[0];
+                                obj.abstract = eventJson.schedule.day[i].room[j].event[e].abstract[0];
+                                events.push(obj);
+                                //db.events.save(obj, function(err, saved) {});
+                            }
                         }
                     }
                 }
